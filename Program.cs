39f7024ae69d8c68831c935061d6682777ee95d4
Piers Karsenbarg <piers@pulumi.com>
2024-01-26 14:40:10 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.EventHub.V20230101Preview;
+using Pulumi.AzureNative.EventHub.V20230101Preview.Inputs;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    // Create an Azure Resource Group
+    var resourceGroup = new ResourceGroup("eventhub-rg");
+
+    var mynamespace = new Namespace("namespace", new()
+    {
+        IsAutoInflateEnabled = true,
+        MaximumThroughputUnits = 40,
+        MinimumTlsVersion = "1.2",
+        PublicNetworkAccess = "Enabled",
+        ResourceGroupName = resourceGroup.Name,
+        Sku = new SkuArgs
+        {
+            Capacity = 1,
+            Name = "Standard",
+            Tier = "Standard"
+        },
+        Tags = new InputMap<string>
+        {
+            {"Name2", "mynamespace"}
+        },
+        ZoneRedundant = true
+    });
+});
